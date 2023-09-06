c076598e4b3c98b9aec1bf79f51f6c3e40059e6f
Vitor - PC <vitor.email2011@gmail.com>
2023-09-06 13:34:58 -0300

    importando e usando novo componente

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HomeComponent,
+    HousingLocationComponent,
   ],
   template: `
   <main>
@@ -17,6 +19,8 @@ import { HomeComponent } from './home/home.component';
     <section class="content">
       <app-home></app-home>
     </section>
+
+    <app-housing-location/>
     
   </main>
 `,

6ae848e6f60dcfb6c9958a26a5387a13ecba04b9
Vitor - PC <vitor.email2011@gmail.com>
2023-09-07 09:43:12 -0300

    criando routerlink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HomeComponent,
-    RouterModule,
+    RouterLink,
+    RouterOutlet,
   ],
   template: `
   <main>

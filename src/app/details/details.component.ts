3fae93b41f09e34dd918ead103acad14814bd7dc
Vitor - PC <vitor.email2011@gmail.com>
2023-09-08 10:15:56 -0300

    consumir dados do server

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -53,8 +53,10 @@ export class DetailsComponent {
   housingLocation: HousingLocation | undefined;
 
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
   }
 
   applyForm = new FormGroup(
